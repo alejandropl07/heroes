49a713c38361f25ffcc4360fcf7063cd6be06c2a
alejandropl07 <aprietoleon97@gmail.com>
2022-07-04 17:53:01 -0500

    Hero Card

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -1,17 +1,16 @@
 import React from "react";
+import HeroCard from "./HeroCard";
 import { getHeroesByPublisher } from "../../helpers/getHeroesByPublisher";
 
 const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
 
   return (
-    <>
-      <ul>
-        {heroes.map((hero) => (
-          <li key={hero.id}>{hero.superhero}</li>
-        ))}
-      </ul>
-    </>
+    <div className="row rows-cols-1 row-cols-md-3 g-3 animate__animated animate__fadeIn">
+      {heroes.map((hero) => (
+        <HeroCard key={hero.id} {...hero} />
+      ))}
+    </div>
   );
 };
 
