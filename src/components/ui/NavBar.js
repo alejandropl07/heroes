a44ebbd6d21dedc6067d424090b7e442c5b09c2c
alejandropl07 <aprietoleon97@gmail.com>
2022-07-08 14:24:04 -0500

    Logout

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,12 +1,16 @@
 import { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../auth/authContext";
+import { types } from "../../types";
 
 const NavBar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, dispatch } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogout = () => {
+    dispatch({
+      type: types.logout,
+    });
     navigate("/login", {
       replace: true,
     });
